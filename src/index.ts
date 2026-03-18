e2c7cf7f675dffc2c85c8e6ea66b3cde14612083
GameMindPilot <k8techent@gmail.com>
2026-03-18 10:33:32 +0530

    chore: Final Viral Growth Release v2.9.1 - Category Creator Edition

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const program = new Command();
 program
   .name('gmpilot')
   .description('GameMindPilot CLI - Your AI Game Development Assistant')
-  .version('2.9.0');
+  .version('2.9.1');
 
 import { loginCommand } from './commands/login';
 import { chatCommand } from './commands/chat';
